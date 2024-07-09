24f48d75a35b289182aec3c97438a60d8e95ec57
arpithajare <arpithajare9@gmail.com>
2024-07-09 16:57:17 +0530

    Updates

---
+++ precache-manifest.c550d2ea68582eff42901e84e9eac95d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12f787123827ef8ececdf00c47968b6",
+    "url": "/temp1/index.html"
+  },
+  {
+    "revision": "2ec7bc838f1199151dbc",
+    "url": "/temp1/static/css/main.1bc47f96.chunk.css"
+  },
+  {
+    "revision": "07f4c2ce35b7cf2690dc",
+    "url": "/temp1/static/js/2.1faf487c.chunk.js"
+  },
+  {
+    "revision": "2ec7bc838f1199151dbc",
+    "url": "/temp1/static/js/main.9bea90b3.chunk.js"
+  },
+  {
+    "revision": "f91002bc142982f2b448",
+    "url": "/temp1/static/js/runtime~main.443f1d7d.js"
+  }
+]);
